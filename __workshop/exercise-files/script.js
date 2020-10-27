a5173d5a893a20f16c7c3d5f47426e0a171bdfdd
FX Cyr <fxtest@MBP-de-Francois.cogeco.local>
2020-10-26 20:50:04 -0400

    75% work done

--- __workshop/exercise-files/script.js
+++ __workshop/exercise-files/script.js
@@ -1,29 +1,46 @@
-let formElement = document.getElementById("#form")
-let passwordInput = document.getElementById("#password")
-let confirmPwInput= document.getElementById("#confirmpassword")
-let termsInput = document.getElementById("#termsandservices")
-let clearButton = document.getElementById("#clear")
-let submitButton = document.getElementById("#submit")
-let errorBox = document.getElementById("#errormessage")
+let formElement = document.querySelector("#form")
+let passwordInput = document.querySelector("#password")
+let confirmPwInput= document.querySelector("#confirm-password")
+let termsInput = document.querySelector("#termsandservices")
+let clearButton = document.querySelector("#clear")
+let submitButton = document.querySelector("#submit")
+let errorBox = document.querySelector("#error-message")
 
-let checkIfValid = (event) => {
-    event.preventDefault()
 
-    formModification()
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    let password = passwordInput.value
+    let confirmpw = confirmPwInput.value
+
+    if (termsInput.checked === false){
+        window.alert("Please agree to the terms & services before continuing.")
+        return;
+    }
  
-    if (passwordInput.value.length < 10) {
-    errorBox.style.display = "block"
-    errorBox.innerText = "Your password is too short! Please provide a password that is at least 10 characters long."
-    passwordInput.style.border = "1px solid red"
+
+  
+    else if (password.length < 10) {
+passwordInput.style.border ="1px solid pink"
+passwordInput.focus()
+        errorBox.innerText = "Your password is too short! Please provide a password that is at least 10 characters long."
+        errorBox.style.display = "block"
+
+    }
+    
+
+    else if (confirmpw !== password) {
+        confirmPwInput.style.border ="1px solid pink"
+        confirmPwInput.focus()
+        errorBox.innerText = "Your password does not match."
+        errorBox.style.display = "block"
     }
 
-    else if (passwordInput.value !== confirmPwInput.value) {
-    errorBox.style.display = "block"
-    errorBox.innerText = "Your password does not match! Please re-confirm password."
-    confirmPwInput.classList.add("errorOutlined")
-    }
+
 
     else {
         window.alert("Success!")
     }
 }
+
+form.addEventListener("submit", handleSubmit)
